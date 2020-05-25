09ec740b6797506f6775e6339340783ae52c69a4
Eaglepants69 <45204280+Eaglepants69@users.noreply.github.com>
2020-05-25 12:02:01 +0200

    Add files via upload

--- scale.php
+++ scale.php
@@ -2,10 +2,10 @@
     $balans1 = 1;
     $balans2 = 1;
 
-if ($balans1 == $balans2) {
+if ($balans1 == $balans2) { 
     echo "in balans" ;  
 
-} 
+}                       
 
 else {
     echo "niet in balans" ;
